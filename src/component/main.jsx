959a220e4b9e845d1a3690eba8eae3e60d53abe9
Mupat <dev@mupat.net>
2017-03-20 15:42:01 +0100

    add table representation for log file

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -5,16 +5,18 @@ class NginxParser extends React.Component {
     super(props);
     this.worker = this.props.worker;
     this.file = this.file.bind(this);
+    this.state = {
+      content: []
+    }
 
     this.worker.addEventListener('message', this.message.bind(this), false);
   }
 
   message(event) {
-    console.log('Worker said: ', event.data);
+    this.setState({ content: event.data });
   }
 
   file(event) {
-    console.log(event.target.files[0]);
     this.worker.postMessage({action: 'read', file: event.target.files[0]});
   }
 
@@ -23,6 +25,24 @@ class NginxParser extends React.Component {
       <div>
         <h1> Nginx Log Parser </h1>
         <input type='file' onChange={this.file} />
+        <table>
+          <thead>
+            <tr>
+              {this.state.content[0] ? Object.keys(this.state.content[0]).map((key, i) =>
+                <th key={`header-${i}`}>{key}</th>
+              ) : ''}
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.content.map((line, i) =>
+              <tr key={`line-${i}`}>
+                {Object.keys(line).map((key, j) =>
+                  <td key={`entry-${j}`}>{line[key]}</td>
+                )}
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
     )
   }
